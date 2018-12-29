a78ad455ec011e7561172f2b562966ed92b57364
Anton <kroshilin.a@yandex.ru>
2018-12-29 16:53:22 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ if [[ $XDEBUG_ENABLED == true ]]; then
     # enable xdebug remote config
     echo "[xdebug]" |  tee -a /usr/local/etc/php/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.remote_enable=1" |  tee -a /usr/local/etc/php/conf.d/00_xdebug.ini > /dev/null
-    echo "xdebug.remote_host=10.254.254.254" |  tee -a /usr/local/etc/php/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.remote_host=host.docker.internal" |  tee -a /usr/local/etc/php/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.remote_port=9005" |  tee -a /usr/local/etc/php/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.scream=0" |  tee -a /usr/local/etc/php/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.cli_color=1" |  tee -a /usr/local/etc/php/conf.d/00_xdebug.ini > /dev/null
